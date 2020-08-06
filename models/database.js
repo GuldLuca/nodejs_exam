2329db9fad9fb7616109122dda8aaee1f3b984c9
Luca <bastholm.luca@gmail.com>
2020-08-06 16:27:51 +0200

    Fix require statement

--- models/database.js
+++ models/database.js
@@ -4,7 +4,7 @@ const Sequelize = require("sequelize");
 let sequelize = undefined;
 
 if (os.hostname() === "web4") {
-  sequelize = require("database.prod.js");
+  sequelize = require("./database.web4.js");
 } else {
   sequelize = new Sequelize("chatroom", "root", "password", {
     dialect: "mysql",
